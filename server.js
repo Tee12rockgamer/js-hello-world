1c7f6e2cdf93afde222d54f412125d404985d8a2
Tee12rockgamer <Phuvanat.12@gmail.com>
2022-08-28 04:15:57 +0000

    update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<h1>สวัสดีมหาลัย</h1>')
+  return res.send('<h1>สวัสดีมหาลัยTee12</h1>')
 })
 
 const PORT = process.env.PORT || 3000
